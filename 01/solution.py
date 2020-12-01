a71091213cb1862df3c9b1ab3cd38d805fd57185
LILITH <evilgamerx42@gmail.com>
2020-12-01 18:12:17 -0500

    Adding code for part 2 of the problem
    - It's the same thing, make a tripple pair of all the combinations of numbers, find the one that is 2020, then win

--- 01/solution.py
+++ 01/solution.py
@@ -14,3 +14,20 @@ for pair in doubled:
         break
 
 print(f'The answer for the double is: {doubleAnswer}')
+
+triple = []
+
+for i, x in enumerate(content):
+    i+=1
+    for j, y in enumerate(content[i:]):
+        j+=1
+        for z in content[j:]:
+            triple.append((int(x), int(y), int(z)))
+
+for pair in triple:
+    if(pair[0] + pair[1] + pair[2] == 2020):
+        tripleAnswer = pair[0] * pair[1] * pair[2]
+        break
+
+
+print(f'The answer for the triple is: {tripleAnswer}')
