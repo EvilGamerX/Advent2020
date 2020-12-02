2aaea58111585866deeabe977be33d9a4408c62d
LILITH <evilgamerx42@gmail.com>
2020-12-02 08:16:21 -0500

    Part 2, XORs are fun

--- 02/solution.py
+++ 02/solution.py
@@ -11,3 +11,16 @@ for line in content:
         validCount += 1
 
 print(f'The number of valid passwords are: {validCount}')
+
+# Part 2
+validCount = 0
+
+for line in content:
+    spaceSplit = line.split(' ')
+    locations = [int(x) for x in spaceSplit[0].split('-')]
+    checkLetter = spaceSplit[1][0]
+    if (spaceSplit[2][locations[0] - 1] == checkLetter) ^ (spaceSplit[2][locations[1] - 1] == checkLetter):
+        validCount += 1
+
+
+print(f'The number of valid passwords are: {validCount}')
