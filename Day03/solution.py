17bb795395ed04d0d56045c6779062074dfbaeec
LILITH <evilgamerx42@gmail.com>
2020-12-03 09:36:20 -0500

    Update for part 2
    Check everything at once, so have a list of movements, positions and trees
    Because the last one only moves every other line (2 down) only increment right when you move

--- Day03/solution.py
+++ Day03/solution.py
@@ -1,3 +1,5 @@
+import math
+
 with open('./Day03/input.txt') as f:
         content = [i.strip() for i in f.readlines()]
 
@@ -12,3 +14,27 @@ for row in content:
 
 print(f'The number of trees you hit are: {trees}')
 
+rightJumps = [1, 3, 5, 7, 1]
+downMods = [1, 1, 1, 1, 2]
+trees = [0, 0, 0, 0, 0]
+rightPositions = [0, 0, 0, 0, 0]
+
+
+for i, row in enumerate(content):
+    if row[rightPositions[0]] == '#':
+        trees[0] += 1
+    if row[rightPositions[1]] == '#':
+        trees[1] += 1
+    if row[rightPositions[2]] == '#':
+        trees[2] += 1
+    if row[rightPositions[3]] == '#':
+        trees[3] += 1
+    if not bool(i % downMods[4]):
+        if row[rightPositions[4]] == '#':
+            trees[4] += 1
+        rightPositions[4] = (rightPositions[4] + rightJumps[4]) % loopMod
+        
+    for x in range(0, 4):
+        rightPositions[x] = (rightPositions[x] + rightJumps[x]) % loopMod
+        
+print(f'The product of trees you hit are: {math.prod(trees)}')
